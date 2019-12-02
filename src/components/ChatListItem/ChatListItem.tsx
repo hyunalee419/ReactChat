e79372ae9fa2da3200649666041fde4f667490ce
hyuna <dlgusdk419@naver.com>
2019-12-02 22:49:07 +0900

    feat(room): add ChatListItem

---
+++ src/components/ChatListItem/ChatListItem.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import styled from "styled-components";
+
+export default ({
+  children,
+	send = false
+}: {
+	children: React.ReactNode,
+	send?: boolean
+}) => (
+	<Li className={send ? 'send-message' : ''}><Text>{children}</Text></Li>
+);
+
+const Li = styled.li`
+  &.send-message {
+    text-align: right;
+    
+    div {
+    	background-color: var(--color-purple);
+    	bax-shadow: 0 2px 4px 0 rgba(91, 56, 177, 0.4);
+    	color: var(--color-white);
+    }
+  }
+`;
+
+const Text = styled.div`
+  margin: 5px 0;
+  display: inline-block;
+  padding: 12px;
+	border-radius: 12px;
+  background-color: var(--color-white);
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  color: var(--color-charcoal-grey-two);
+  font-size: 14px;
+  font-weight: 600;
+  letter-spacing: -0.1px;
+`;
--- src/containers/room/ChatList.tsx
+++ src/containers/room/ChatList.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import ChatListItem from 'components/ChatListItem';
 
 export default () => (
-	<ul>
-		<li>출근했니?</li>
-		<li>출근했냐구?</li>
-		<li>어딘데 출근안하니, 죽고싶니?</li>
-		<li>해외 출장중입니다.</li>
-	</ul>
+	<Ul>
+		<ChatListItem>출근했니?</ChatListItem>
+		<ChatListItem>출근했냐구?</ChatListItem>
+		<ChatListItem>어딘데 출근안하니, 죽고싶니?</ChatListItem>
+		<ChatListItem send>해외 출장중입니다.</ChatListItem>
+	</Ul>
 );
+
+const Ul = styled.ul`
+  height: calc(100% - 59px);
+  overflow-y: scroll;
+`;

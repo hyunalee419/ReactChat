ce5d87a2c640f41805a30dd313c3100c31eeb630
hyuna <dlgusdk419@naver.com>
2019-12-02 16:40:32 +0900

    style(list): add imgbutton for header

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import ImgButton from 'components/common/ImgButton';
 
 const StyledDiv = styled.div`
   width: 100%;
   height: 44px;
   background-color: var(--color-purple);
+  text-align: center;
+  color: var(--color-white);
+  font-size: 17px;
+  font-weight: bold;
+  line-height: 44px;
+  letter-spacing: -0.12px;
 `;
 
 export default ({
@@ -13,8 +20,14 @@ export default ({
   right=undefined
 }) => (
   <StyledDiv>
-    <div style={{ float: 'left', padding: '12px 0 0 10px'}}>left</div>
+    <ImgButton
+      buttonProps={{ style: { float: 'left', padding: '12px 0 0 10px' } }}
+      imgProps={{ src: '/menu@3x.png', style: { width: 24, height: 24 } }}
+    />
     {title}
-    <div style={{ float: 'right', padding: '10px 12px' }}>right</div>
+    <ImgButton
+      buttonProps={{ style: { float: 'right', padding: '10px 12px' } }}
+      imgProps={{ src: '/profile@3x.png', style: { width: 24, height: 24 } }}
+    />
   </StyledDiv>
 );

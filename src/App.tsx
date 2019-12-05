243b8274671b23452b83b205dfdbdf26f40332bb
hyunalee <dlgusdk419@naver.com>
2019-12-05 11:49:51 +0900

    feat(transition): add react-router-transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { AnimatedRoute } from 'react-router-transition';
 import List from 'pages/List';
 import Room from 'pages/Room';
 
-
 export default () => (
   <BrowserRouter>
-    <Route exact path="/list" component={List} />
-    <Route exact path="/room/:id" component={Room} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/list" component={List} />
+      {/*<Route exact path="/room/:id" component={Room} />*/}
+      <AnimatedRoute
+        path="/room/:id"
+        component={Room}
+        atEnter={{ offset: 100 }}
+        atLeave={{ offset: 100 }}
+        atActive={{ offset: 0 }}
+        mapStyles={(styles) => ({
+          transform: `translateX(${styles.offset}%)`,
+        })}
+      />
   </BrowserRouter>
 );

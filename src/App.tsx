a5ee4aaa03b70e4e5a07d0f67149d09e20d25a82
hyunalee <dlgusdk419@naver.com>
2019-12-05 11:07:43 +0900

    feat: add Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import List from 'pages/List';
 import Room from 'pages/Room';
 
 
 export default () => (
   <BrowserRouter>
+    <Route exact path="/" component={Home} />
     <Route exact path="/list" component={List} />
     <Route exact path="/room/:id" component={Room} />
   </BrowserRouter>
+);
+
+const Home = () => (
+  <Link to="/list">채팅 목록 페이지</Link>
 );
